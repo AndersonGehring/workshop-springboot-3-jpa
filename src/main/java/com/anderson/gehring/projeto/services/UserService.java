d628d24637ecb93b9b81fc0df67a567704cdf07f
Anderson Gehing <andersondrive15@gmail.com>
2023-10-25 18:43:22 -0300

    Service layer, component registration

---
+++ src/main/java/com/anderson/gehring/projeto/services/UserService.java
@@ -0,0 +1,26 @@
+package com.anderson.gehring.projeto.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.anderson.gehring.projeto.entities.User;
+import com.anderson.gehring.projeto.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
